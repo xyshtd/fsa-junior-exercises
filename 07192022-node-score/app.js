5b1172ecec7b3f4152f30519b5145b820dddc8aa
xyshtd <mhu225@gmail.com>
2022-07-22 18:13:37 -0400

    solutions

--- 07192022-node-score/app.js
+++ 07192022-node-score/app.js
@@ -1,23 +1,28 @@
-const scoreFns = require('./scoreFns');
-const fs = require('fs');
+//terminal: node app.js
+const scoreFns = require('./scoreFns');//internal file need ./
+const fs = require('fs');//external 3rd party: The Node.js File System module
 
+//aync operation
+//fs.readFile(path,callback which takes two parameters: 1)err: If any error occurred; 2)data: Contents of the file.
 fs.readFile('class1.txt', (err, data)=> {
   if(err){
     console.log(err);
   }
   else {
-    const lines = data.toString().trim().split('\n');
-    const users = lines.map( line => {
-      const parts = line.split(',');
-      const scores = parts.slice(1);
-      console.log(scores);
+    //Stringify the buffer
+    const lines = data.toString().trim().split('\n');//[ 'moe,68,92,77', 'lucy,99,88,92', 'curly,65,68' ]
+      const users = lines.map(line => {
+        const parts = line.split(',');//[ 'moe', '68', '92', '77' ]
+        const scores = parts.slice(1);//[ '68', '92', '77' ]
+     
       return {
         name: parts[0],
         averageScore: scoreFns.computeAverage(scores) 
       };
-    });
+    }); 
     console.log(users);
   }
 });
 
+//execute before async
 console.log('hello world');
---
+++ 07212022-wizard-news/views/postDetails.js
@@ -0,0 +1,21 @@
+module.exports = function postDetails(post){
+    return `<!DOCTYPE html>
+    <html>
+    <head>
+      <title>${post.title}</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+      <a href="/"><header><img src="/logo.png"/>Wizard News</header></a>
+          <div class='news-item'>
+            <p>
+              ${post.title}
+              <small>(by ${post.name})</small>
+            </p>
+            <p>${post.content}</p>
+          </div>
+      </div>
+    </body>
+  </html>`;
+}
